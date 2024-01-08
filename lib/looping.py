9346ec63a3702987d70d398d8631e206b67a937e
KI JONG BYUN <kjbyun0@gmail.com>
2024-01-08 02:52:42 -0600

    Assignment Done

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print('Happy New Year!')
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [num * num for num in int_list]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        mod_3 = i % 3
+        mod_5 = i % 5
+        if mod_3 == 0 and mod_5 == 0:
+            print('FizzBuzz')
+        elif mod_3 == 0:
+            print('Fizz')
+        elif mod_5 == 0:
+            print('Buzz')
+        else:
+            print(i)
